f5b1cd9189caf7dd108dec6ab4f09afad55fbfd3
Rafael <76231028+rafaelvp17@users.noreply.github.com>
2021-11-29 19:06:07 -0500

    Agregar archivos de proyecto.

---
+++ LlamandoDesdeForms/frmCallFunciones.Designer.cs
@@ -0,0 +1,183 @@
+﻿
+namespace LlamandoDesdeForms
+{
+    partial class frmCallFunciones
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResSum = new System.Windows.Forms.TextBox();
+            this.txtFac = new System.Windows.Forms.TextBox();
+            this.txtResFac = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el primer valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(285, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el segundo valor:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(241, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ingrese el factorial:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(230, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado factorial:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 147);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(241, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Resultado de la suma:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalcular.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(553, 207);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(181, 57);
+            this.btnCalcular.TabIndex = 11;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(347, 75);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(215, 22);
+            this.txtNum1.TabIndex = 12;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(347, 112);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(215, 22);
+            this.txtNum2.TabIndex = 13;
+            this.txtNum2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtResSum
+            // 
+            this.txtResSum.Location = new System.Drawing.Point(347, 149);
+            this.txtResSum.Name = "txtResSum";
+            this.txtResSum.Size = new System.Drawing.Size(215, 22);
+            this.txtResSum.TabIndex = 14;
+            // 
+            // txtFac
+            // 
+            this.txtFac.Location = new System.Drawing.Point(259, 210);
+            this.txtFac.Name = "txtFac";
+            this.txtFac.Size = new System.Drawing.Size(215, 22);
+            this.txtFac.TabIndex = 15;
+            // 
+            // txtResFac
+            // 
+            this.txtResFac.Location = new System.Drawing.Point(248, 244);
+            this.txtResFac.Name = "txtResFac";
+            this.txtResFac.Size = new System.Drawing.Size(215, 22);
+            this.txtResFac.TabIndex = 16;
+            // 
+            // frmCallFunciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResFac);
+            this.Controls.Add(this.txtFac);
+            this.Controls.Add(this.txtResSum);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCallFunciones";
+            this.Text = "Llamar funciones";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResSum;
+        private System.Windows.Forms.TextBox txtFac;
+        private System.Windows.Forms.TextBox txtResFac;
+    }
+}
+
---
+++ LlamandoDesdeForms/frmCallFunciones.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LlamandoDesdeForms
+{
+    public partial class frmCallFunciones : Form
+    {
+        public frmCallFunciones()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnCalcular_Click(object sender, EventArgs e)
+        {
+            BibliotecadeFunciones.Operaciones ob1 = new BibliotecadeFunciones.Operaciones();
+
+            double num1 = Convert.ToDouble(this.txtNum1.Text);
+            double num2 = Convert.ToDouble(this.txtNum2.Text);
+            this.txtResSum.Text = ob1.suma(num1, num2).ToString();
+
+            double fact = double.Parse(this.txtFac.Text);
+            this.txtResFac.Text = ob1.factorial(fact).ToString();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
